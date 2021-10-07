edb6c10a20a89ec49530a1b05c699a866ea536b8
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2021-10-07 08:13:44 -0300

    feat: add calculator util

---
+++ src/utils/calculadora.py
@@ -0,0 +1,9 @@
+class CalculadoraUtil:
+    def somar(self, argumento1:float, argumento2:float):
+        return argumento1 + argumento2
+
+    def dividir(elf, argumento1:float, argumento2:float):
+        return argumento1 / argumento2
+
+    # TODO: adicionar método `subtrair`
+    # TODO: adicionar método `multiplicar`
